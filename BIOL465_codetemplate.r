a034d9792408d0e2ffbf52b28dae13dd381f9186
tomas <ttomas@email.unc.edu>
2023-11-14 10:28:20 -0500

    add template to repod

---
+++ BIOL465_codetemplate.r
@@ -0,0 +1,66 @@
+######################################
+# BIOL 465 Research Project
+# Your Names Here
+# Date
+# Short project title
+######################################
+
+# Use this R script file to document how you proceed from 
+# your raw data files to your processed (e.g. aggregated and/or merged) 
+# data to analyses and plots. Use comments to create a road map for 
+# everything that you will need to do. Some general steps that might 
+# be useful are below, but it is only a guide and your own set of 
+# steps required may be different.
+
+# Be as specific as possible with respect to which fields are being 
+# manipulated and what exactly is being calculated when writing your comments.
+
+
+
+### 0. Load any libraries you might need, e.g. dplyr, vioplot, raster
+#   You do NOT need to specify install.packages(), just library()
+
+
+
+
+### 1. Read in raw data files
+#   Mention briefly the source of these data files (e.g. provide link)
+#   and what kind of data each file contains in comments
+
+
+
+### 2. Dataset exploration
+#   Make sure you understand the dataset. Try making histograms or boxplots of your data.
+#   Use summary() on your data objects to quickly see what formats each column are in.
+
+
+
+### 3. Simplify data (subset to the columns and/or rows of interest)
+
+
+
+### 4. Calculate metrics of interest (e.g. mean abundance per species, or 
+#   species richness per site, etc.)
+
+
+
+### 5. Combine relevant information from each of your two or more datasets
+#   using left_join() (or full_join or inner_join)
+
+
+
+### 6. Conduct analyses (linear regression, variance partitioning, etc)
+
+
+
+### 7. Make plots
+
+
+
+
+
+#-------------------------------------------------------------------------------#
+# One basic test of your code is whether you can open a new RStudio window, and #
+# remove all existing objects with the 'broom' in the top right, and then run   #
+# the whole script without getting an error.                                    #
+#------------------------------------------------------------------------------#
