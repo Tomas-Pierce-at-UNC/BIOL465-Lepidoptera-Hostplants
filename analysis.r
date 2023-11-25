69c27f8429a00f090fad729517be7f3fff2eab25
tomas <ttomas@email.unc.edu>
2023-11-25 15:34:10 -0500

    checked everything I needed to check

--- analysis.r
+++ analysis.r
@@ -6,14 +6,23 @@ library(ggplot2)
 # machine learning libs
 library(e1071)
 library(caret)
-library(xgboost)
 library(randomForest)
 
 source("utils.r")
 
 hosts2 <- load_hosts()
 envelope2 <- load_envelope()
+
+num_env <- envelope2[,c(13:24)]
+env_pca <- princomp(num_env)
+plot(env_pca)
+
 leptraits2 <- load_leptraits()
+
+wings <- leptraits2[,c(10,11)]
+wings <- wings[!is.na(wings)]
+wing_pca <- princomp(wings)
+plot(wing_pca)
 
 # ignore hybrids
 envelope3 <- envelope2[is.na(envelope2$Plant.SubSpecies1),]
@@ -46,6 +55,21 @@ grouped_envelopes <- by_insect %>% summarize(
   male_ws_lower = mean(WS_L_Mal),
   
 )
+
+grouped_envelopes$is_specialist <- grouped_envelopes$number_hosts <= 2
+
+grouped_envelopes <- grouped_envelopes[!is.na(grouped_envelopes$wingspan_upper),]
+
+grouped_envelopes$temp_width = grouped_envelopes$temp_max - grouped_envelopes$temp_min
+
+vp <- variance_partition(grouped_envelopes, "wingspan_upper", "precip_avg", "temp_max")
+plot_variance_partion_pie(vp)
+
+vp <- variance_partition(grouped_envelopes, "wingspan_upper", "temp_min", "temp_max")
+plot_variance_partion_pie(vp)
+
+vp <- variance_partition(grouped_envelopes, "wingspan_upper", "temp_min", "precip_avg")
+plot_variance_partion_pie(vp)
 
 # testing overall hypothesis part 1: max temperature
 model1 <- lm(wingspan_upper ~ temp_max, grouped_envelopes)
@@ -80,6 +104,8 @@ p1 <- ggplot(grouped_envelopes, aes(precip_avg, wingspan_upper)) +
 print(p1)
 print(summary(model1))
 
+# specialists versus generalists
+
 # setup for generalist vs specialist ANCOVA
 grouped_envelopes$is_specialist <- as.numeric(grouped_envelopes$number_hosts <= 2)
 
@@ -110,6 +136,8 @@ p2 <- ggplot(grouped_envelopes, aes(precip_avg, wingspan_upper, colour = as.fact
 print(p2)
 print(summary(specialization.ancova))
 
+# sex comparisons
+
 # direct comparison of male and female lepidopterans requires reformatting data
 masculine <- grouped_envelopes[,c(2,3,4,7,10,12)]
 feminine <- grouped_envelopes[,c(2,3,4,7,8,12)]
@@ -124,11 +152,60 @@ recombined <- base::rbind(masculine, feminine)
 sexed_model <- lm(sexed_wingspan ~ male_sex +  temp_max + male_sex*temp_max, recombined)
 print(summary(sexed_model))
 
+ggplot(recombined, aes(temp_max, sexed_wingspan, color=as.factor(male_sex))) + 
+  geom_point() + 
+  geom_smooth(method="lm") +
+  theme_bw() +
+  xlab("Maximum Temperature") +
+  ylab("Sexed Upper Wingspan")
+
 # analysis of covariance, min temp
 sexed_model <- lm(sexed_wingspan ~ male_sex +  temp_min + male_sex*temp_min, recombined)
 print(summary(sexed_model))
 
+ggplot(recombined, aes(temp_min, sexed_wingspan, color=as.factor(male_sex))) + 
+  geom_point() + 
+  geom_smooth(method="lm") +
+  theme_bw() +
+  xlab("Minimum Temperature") +
+  ylab("Sexed Upper Wingspan")
+
 # analysis of covariance, precipitation
 sexed_model <- lm(sexed_wingspan ~ male_sex + precip_avg + male_sex*precip_avg, recombined)
 print(summary(sexed_model))
+
+ggplot(recombined, aes(precip_avg, sexed_wingspan, color=as.factor(male_sex))) + 
+  geom_point() + 
+  geom_smooth(method="lm") +
+  theme_bw() +
+  xlab("Average Precipitation") +
+  ylab("Sexed Upper Wingspan")
+
+
+# can we get specialization from wingspan data?
+grouped_envelopes$is_specialist <- as.factor(grouped_envelopes$number_hosts <= 2)
+train <- grouped_envelopes %>% dplyr::sample_frac(0.7)
+test <- dplyr::anti_join(grouped_envelopes, train)
+rmodel <- randomForest(is_specialist ~ wingspan_upper + wingspan_lower, train, na.action=na.omit)
+predictions <- predict(rmodel, test)
+cm <- confusionMatrix(predictions, test$is_specialist)
+print(cm)
+
+# can we get specialization from temperature data?
+grouped_envelopes$is_specialist <- as.factor(grouped_envelopes$number_hosts <= 2)
+train <- grouped_envelopes %>% dplyr::sample_frac(0.7)
+test <- dplyr::anti_join(grouped_envelopes, train)
+rmodel <- randomForest(is_specialist ~ temp_max + temp_min, train, na.action=na.omit)
+predictions <- predict(rmodel, test)
+cm <- confusionMatrix(predictions, test$is_specialist)
+print(cm)
+
+# can we get specialization from temperature and wingspan data?
+grouped_envelopes$is_specialist <- as.factor(grouped_envelopes$number_hosts <= 2)
+train <- grouped_envelopes %>% dplyr::sample_frac(0.7)
+test <- dplyr::anti_join(grouped_envelopes, train)
+rmodel <- randomForest(is_specialist ~ temp_max + temp_min + wingspan_upper + wingspan_lower, train, na.action=na.omit)
+predictions <- predict(rmodel, test)
+cm <- confusionMatrix(predictions, test$is_specialist)
+print(cm)
 
--- utils.r
+++ utils.r
@@ -27,3 +27,62 @@ load_leptraits <- function() {
   return(leptraits2)
 }
 
+# reproduced from Homework 5 assignment
+# function to run variance partioning of a dataframe.
+# will determine what amount of variation in resp_col_name column
+# in dframe is attributable to in1_col_name column, 
+# in2_col_name column, their combination, and neither.
+# works by fitting linear models and taking the differences of
+# their R-squared values
+variance_partition <- function(dframe, resp_col_name, in1_col_name, in2_col_name) {
+  # extract relevant features
+  response = dframe[[resp_col_name]]
+  input1 = dframe[[in1_col_name]]
+  input2 = dframe[[in2_col_name]]
+  
+  # create linear models
+  shared_model = lm(response ~ input1 + input2)
+  input1_model = lm(response ~ input1)
+  input2_model = lm(response ~ input2)
+  
+  #display models
+  sm = summary(shared_model)
+  in1 = summary(input1_model)
+  in2 = summary(input2_model)
+  
+  print(c(resp_col_name, "=", in1_col_name, "+", in2_col_name))
+  print(sm)
+  print(c(resp_col_name, "=", in1_col_name))
+  print(in1)
+  print(c(resp_col_name, "=", in2_col_name))
+  print(in2)
+  
+  # get explained variations
+  shared_model.variation = sm$r.squared
+  input1_model.variation = in1$r.squared
+  input2_model.variation = in2$r.squared
+  
+  # extract uniquely attributable variations
+  unique_input2 <- shared_model.variation - input1_model.variation 
+  unique_input1 <- shared_model.variation - input2_model.variation
+  not_attributable <- 1 - shared_model.variation
+  both_inputs <- input1_model.variation + input2_model.variation - shared_model.variation
+  
+  # collect data
+  variations <- c(unique_input1, unique_input2, both_inputs, not_attributable)
+  
+  labels <- c(in1_col_name, in2_col_name, "both inputs", "neither input")
+  
+  variations_with_labels <- list(variations, labels)
+  
+  #return output data
+  return(variations_with_labels)
+}
+
+# reproduced from HW 5
+plot_variance_partion_pie <- function(vars_with_labels) {
+  variations = vars_with_labels[[1]]
+  labels = vars_with_labels[[2]]
+  pie(variations, labels)
+  title("Variance Partion Proportions")
+}
